43873b431d2413122b95f92c884456e76fadd3eb
Sandeep M S <sandeepms746@gmail.com>
2023-07-24 11:58:52 +0530

    added responsive navbar

--- src/components/KenButton/index.jsx
+++ src/components/KenButton/index.jsx
@@ -1,7 +1,26 @@
 import { Button } from "@mui/material";
-import * as React from "react";
+import React from "react";
 
-export default function KenButton(props) {
-	const { children } = props;
-	return <Button {...props}>{children}</Button>;
+function KenButton(props) {
+  const { title, style, buttonType } = props;
+  switch (buttonType) {
+    case "outlined":
+      return (
+        <Button variant="outlined" sx={style}>
+          {title}
+        </Button>
+      ); 
+
+    case "contained":
+      return (
+        <Button variant="contained" sx={style}>
+          {title}
+        </Button>
+      );
+
+    default:
+      return <Button sx={style}>{title}</Button>;
+  }
 }
+
+export default KenButton;
